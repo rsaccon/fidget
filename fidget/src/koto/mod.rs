488ea191651dd04c4fcd50b6cf2aafb5c0a5fb14
Roberto Saccon <rsaccon@gmail.com>
2025-03-31 05:32:43 +0100

    refactoring

--- fidget/src/koto/mod.rs
+++ fidget/src/koto/mod.rs
@@ -5,6 +5,67 @@ use std::sync::{Arc, Mutex};
 use crate::{context::Tree, Error};
 use koto::{derive::*, prelude::*, runtime};
 
+macro_rules! define_binary_op_fns {
+    ($koto_name:ident, $tree_name:ident) => {
+        fn $koto_name(&self, rhs: &KValue) -> runtime::Result<KValue> {
+            let lhs = self.0.clone();
+            match rhs {
+                KValue::Number(num) => {
+                    let tree = lhs.$tree_name(Tree::constant(f64::from(num)));
+                    Ok(KValue::Object(Self(tree).into()))
+                }
+                KValue::Object(obj) if obj.is_a::<KotoTree>() => {
+                    let koto_tree = obj.cast::<KotoTree>();
+                    let tree = koto_tree.unwrap().to_owned().0;
+                    let result = lhs.$tree_name(tree);
+                    Ok(KValue::Object(Self(result).into()))
+                }
+                unexpected => {
+                    let err_msg = format!(
+                        "invalid type for {}(Tree, rhs)",
+                        stringify!($tree_name)
+                    );
+                    unexpected_type(&err_msg, unexpected)
+                }
+            }
+        }
+    };
+}
+
+#[derive(Clone, KotoCopy, KotoType)]
+#[koto(type_name = "Tree")]
+struct KotoTree(Tree);
+
+impl KotoObject for KotoTree {
+    define_binary_op_fns!(add, add);
+    define_binary_op_fns!(subtract, sub);
+    define_binary_op_fns!(multiply, mul);
+    define_binary_op_fns!(divide, div);
+}
+
+#[koto_impl]
+impl KotoTree {
+    fn make_koto_object(tree: Tree) -> KObject {
+        let koto_tree = Self(tree.into());
+        KObject::from(koto_tree)
+    }
+
+    fn x() -> KObject {
+        let koto_tree = Self(Tree::x().into());
+        KObject::from(koto_tree)
+    }
+
+    fn y() -> KObject {
+        let koto_tree = Self(Tree::y().into());
+        KObject::from(koto_tree)
+    }
+
+    fn z() -> KObject {
+        let koto_tree = Self(Tree::z().into());
+        KObject::from(koto_tree)
+    }
+}
+
 /// Engine for evaluating a Koto script with Fidget-specific bindings
 pub struct Engine {
     engine: Koto,
@@ -41,52 +102,6 @@ impl Engine {
             ])))
         });
 
-        // CAN BE REMOVED, we just export a shape
-        // engine.prelude()
-        //     .add_fn("draw", move |ctx| {
-        //             let args = ctx.args();
-        //             if args.len() != 1 {
-        //                 return unexpected_args("1 argument: shape", args);
-        //             }
-        //             match &args[0] {
-        //                 KValue::Object(obj) if obj.is_a::<KotoTree>() => {
-        //                     // TODO
-        //                     // let ctx = ctx.tag().unwrap().clone_cast::<Arc<Mutex<ScriptContext>>>();
-        //                     // ctx.lock().unwrap().shapes.push(DrawShape {
-        //                     //     tree,
-        //                     //     color_rgb: [u8::MAX; 3],
-        //                     // });
-        //                     Ok(())
-        //                 }
-        //                 unexpected => unexpected_args("wrong argument type", unexpected),
-        //             }
-        //         });
-
-        // CAN BE REMOVED, we just export a shape as part of tupe or list which also contains rgb
-        // engine.prelude()
-        //     .add_fn("draw_rgb", move |ctx| {
-        //         let args = ctx.args();
-        //         if args.len() != 4 {
-        //             return unexpected_args("4 arguments: shape, r, g, b", args);
-        //         }
-        //         // TODO
-        //         // let ctx = ctx.tag().unwrap().clone_cast::<Arc<Mutex<ScriptContext>>>();
-        //         // let f = |a| {
-        //         //     if a < 0.0 {
-        //         //         0
-        //         //     } else if a > 1.0 {
-        //         //         255
-        //         //     } else {
-        //         //         (a * 255.0) as u8
-        //         //     }
-        //         // };
-        //         // ctx.lock().unwrap().shapes.push(DrawShape {
-        //         //     tree,
-        //         //     color_rgb: [f(r), f(g), f(b)],
-        //         // });
-        //         Ok(())
-        //     });
-
         // CAN BE REMOVED, we use remap in KotoTree
         engine.prelude()
             .add_fn("remap_xyz", move |ctx| {
@@ -107,30 +122,17 @@ impl Engine {
                             let result = tree.remap_xyz(x, y, z);
                             Ok(KotoTree::make_koto_object(result).into())
                         } else {
-                            unexpected_args("wrong argument type", args)
+                            unexpected_args("invalid type", args)
                         }
                     }
-                    unexpected => unexpected_args("wrong argument type", unexpected),
+                    _ => unexpected_args("invalid type", args)
                 }
             });
 
-        engine.prelude().add_fn("sqrt", move |ctx| {
-            let args = ctx.args();
-            if args.len() != 1 {
-                return unexpected_args("1 argument: tree or number", args);
-            }
-            match &args[0] {
-                KValue::Object(obj) if obj.is_a::<KotoTree>() => {
-                    let tree = obj.cast::<KotoTree>()?.to_owned().0;
-                    let result = tree.sqrt();
-                    Ok(KotoTree::make_koto_object(result).into())
-                }
-                // TODO: check and handle number
-                _ => unexpected_args("wrong argument type", args),
-            }
-        });
+        engine.prelude().add_fn("sqrt", register_sqrt);
 
         engine.prelude().add_fn("square", move |ctx| {
+            //register_fn!(square);
             let args = ctx.args();
             if args.len() != 1 {
                 return unexpected_args("1 argument: tree or number", args);
@@ -142,7 +144,7 @@ impl Engine {
                     Ok(KotoTree::make_koto_object(result).into())
                 }
                 // TODO: check and handle number
-                _ => unexpected_args("wrong argument type", args),
+                unexpected => unexpected_type("invalid type", unexpected),
             }
         });
 
@@ -156,17 +158,10 @@ impl Engine {
         self.context.lock().unwrap().clear();
 
         match self.engine.compile_and_run(script) {
-            Ok(_) => {
-                // alternative for draw and draw_rgb
-                for (i, (key, value)) in
-                    self.engine.exports().data().iter().enumerate()
-                {
-                    match value {
+            Ok(KValue::List(list)) => {
+                for el in list.data().iter() {
+                    match el {
                         KValue::Object(obj) if obj.is_a::<KotoTree>() => {
-                            println!(
-                                "exported tree - index: {} | name: {}",
-                                i, key
-                            );
                             let koto_tree = obj.cast::<KotoTree>();
                             let tree = koto_tree.unwrap().to_owned().0;
                             self.context.lock().unwrap().shapes.push(
@@ -176,14 +171,13 @@ impl Engine {
                                 },
                             )
                         }
-                        // TODO: check for draw_rgb equivalent
-                        other => {
-                            println!("exported unhandled other: {:#?}", other);
-                            // TODO: create warning ???
-                        }
+                        // TODO: if tuple containing color then do as in rhai viewer
+                        _ => (),
                     }
                 }
             }
+            // TODO: check for single shape (KotoTree object)
+            Ok(_) => println!("No shapes returned"),
             Err(err) => println!("compile error:{}", err),
         }
 
@@ -244,139 +238,18 @@ impl ScriptContext {
     }
 }
 
-// new stuff
-#[derive(Clone, KotoCopy, KotoType)]
-#[koto(type_name = "Tree")]
-struct KotoTree(Tree);
-
-impl KotoObject for KotoTree {
-    fn add(&self, rhs: &KValue) -> runtime::Result<KValue> {
-        let lhs = self.0.clone();
-        match rhs {
-            KValue::Number(num) => {
-                let tree = lhs.add(Tree::constant(f64::from(num)));
-                Ok(KValue::Object(Self(tree).into()))
-            }
-            KValue::Object(obj) if obj.is_a::<KotoTree>() => {
-                let koto_tree = obj.cast::<KotoTree>();
-                let tree = koto_tree.unwrap().to_owned().0;
-                let result = lhs.add(tree);
-                Ok(KValue::Object(Self(result).into()))
-            }
-            // Example from Ohm
-            // (Self::Constant(node), KValue::Number(num)) => {
-            //     Ok(KValue::Object(constant(node.value + f32::from(num)).into()))
-            // }
-            // (_, KValue::Number(num)) => Ok(KValue::Object(
-            //     mix(self.clone(), constant((num).into())).into(),
-            // )),
-            // (_, KValue::Object(obj)) => Ok(KValue::Object(
-            //     mix(self.clone(), obj.cast::<NodeKind>()?.clone()).into(),
-            // )),
-            // Rhai version for expresion: a op b (a is tree, b can be anything)
-            // let b = if let Some(v) = b.clone().try_cast::<f64>() {
-            //     Tree::constant(v)
-            // } else if let Some(v) = b.clone().try_cast::<i64>() {
-            //     Tree::constant(v as f64)
-            // } else if let Some(t) = b.clone().try_cast::<Tree>() {
-            //     t
-            // } else {
-            //     let e = format!(
-            //         "invalid type for {}(Tree, rhs): {}",
-            //         stringify!($name),
-            //         b.type_name()
-            //     );
-            //     return Err(e.into());
-            // };
-            // Ok(a.$name(b))
-            _ => panic!("invalid add operator"),
+fn register_sqrt(ctx: &mut CallContext) -> runtime::Result<KValue> {
+    let args = ctx.args();
+    if args.len() != 1 {
+        return unexpected_args("1 argument: Tree or Number", args);
+    }
+    match &args[0] {
+        KValue::Object(obj) if obj.is_a::<KotoTree>() => {
+            let tree = obj.cast::<KotoTree>()?.to_owned().0;
+            let result = tree.sqrt();
+            Ok(KotoTree::make_koto_object(result).into())
         }
-    }
-
-    fn subtract(&self, rhs: &KValue) -> runtime::Result<KValue> {
-        let lhs = self.0.clone();
-        match rhs {
-            KValue::Number(num) => {
-                let tree = lhs.sub(Tree::constant(f64::from(num)));
-                Ok(KValue::Object(Self(tree).into()))
-            }
-            KValue::Object(obj) if obj.is_a::<KotoTree>() => {
-                let koto_tree = obj.cast::<KotoTree>();
-                let tree = koto_tree.unwrap().to_owned().0;
-                let result = lhs.sub(tree);
-                Ok(KValue::Object(Self(result).into()))
-            }
-            _ => panic!("invalid subtract operator"),
-        }
-    }
-
-    fn multiply(&self, rhs: &KValue) -> runtime::Result<KValue> {
-        let lhs = self.0.clone();
-        match rhs {
-            KValue::Number(num) => {
-                let tree = lhs.mul(Tree::constant(f64::from(num)));
-                Ok(KValue::Object(Self(tree).into()))
-            }
-            KValue::Object(obj) if obj.is_a::<KotoTree>() => {
-                let koto_tree = obj.cast::<KotoTree>();
-                let tree = koto_tree.unwrap().to_owned().0;
-                let result = lhs.mul(tree);
-                Ok(KValue::Object(Self(result).into()))
-            }
-            _ => panic!("invalid multiply operator"),
-        }
-    }
-
-    fn divide(&self, rhs: &KValue) -> runtime::Result<KValue> {
-        let lhs = self.0.clone();
-        match rhs {
-            KValue::Number(num) => {
-                let tree = lhs.div(Tree::constant(f64::from(num)));
-                Ok(KValue::Object(Self(tree).into()))
-            }
-            KValue::Object(obj) if obj.is_a::<KotoTree>() => {
-                let koto_tree = obj.cast::<KotoTree>();
-                let tree = koto_tree.unwrap().to_owned().0;
-                let result = lhs.div(tree);
-                Ok(KValue::Object(Self(result).into()))
-            }
-            _ => panic!("invalid divide operator"),
-        }
-    }
-}
-
-#[koto_impl]
-impl KotoTree {
-    fn make_koto_object(tree: Tree) -> KObject {
-        let koto_tree = Self(tree.into());
-        KObject::from(koto_tree)
-    }
-
-    fn x() -> KObject {
-        let koto_tree = Self(Tree::x().into());
-        KObject::from(koto_tree)
-    }
-
-    fn y() -> KObject {
-        let koto_tree = Self(Tree::y().into());
-        KObject::from(koto_tree)
-    }
-
-    fn z() -> KObject {
-        let koto_tree = Self(Tree::z().into());
-        KObject::from(koto_tree)
-    }
-
-    // A function that returns the object instance as the result
-    #[koto_method]
-    fn draw(ctx: MethodContext<Self>) -> runtime::Result<KValue> {
-        // TODO: check args for rgb
-        let _koto_tree = ctx.instance();
-        // let mut _kmap = ctx.vm.exports_mut();
-        // TODO: create empty exported list "draw"
-        // if let Some(KValue::List(list)) = kmap.get("draw") {
-        //     list.clone().data_mut().push(koto_tree);  // what type to push ???
-        // }
-        ctx.instance_result()
+        // TODO: check and handle number
+        unexpected => unexpected_type("invalid type", unexpected),
     }
 }
